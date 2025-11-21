a5c734da90e2894ea3ff108254921e0ac1a52a22
DEON <40478@ma-web.nl>
2025-11-21 15:25:42 +0100

    update

--- 01_functions/Program.cs
+++ 01_functions/Program.cs
@@ -6,12 +6,56 @@
     {
         Program Program = new Program();
         Program.Run();// er word een niewe program gemaakt en daarna gaat hij het program runnen 
+        Program.vraag1();
+        Program.vraag2();
+        Program.vraag3();
+        Program.vraag4();
+        Program.vraag5();
+        
     } // het is een function. het return type function is
+
     internal void Run()
     {
         Console.WriteLine("hoi");
     }
+    internal void vraag1()
+    {
+        Console.WriteLine("how long do you think you'd survive in a zombie apocalypse?");
+        string antwoord = Console.ReadLine();
+
+        Console.WriteLine(antwoord);
+    }
+        internal void vraag2()
+    {
+        Console.WriteLine("What would you do if you won a million dollars?");
+        string antwoord2 = Console.ReadLine();
+
+        Console.WriteLine(antwoord2);
+    }
+        internal void vraag3()
+    {
+        Console.WriteLine("Have you ever tried talking to a animal?");
+        string antwoord3 = Console.ReadLine();
+
+        Console.WriteLine(antwoord3);
+    }
+        internal void vraag4()
+    {
+        Console.WriteLine("What would you call a male ladybug?");
+        string antwoord4 = Console.ReadLine();
+
+        Console.WriteLine(antwoord4);
+    }
+    internal void vraag5()
+    {
+        Console.WriteLine("Which species would be the rudest if all animals could talk?");
+        string antwoord5 = Console.ReadLine();
+
+        Console.WriteLine(antwoord5);
+    }
+
+
     
 
-    // hier komt de function
+    
 }
