df67a6623de64858edfd773ccd4516de0f56db28
DEON <40478@ma-web.nl>
2025-11-21 14:42:05 +0100

    update

--- 01_functions/Program.cs
+++ 01_functions/Program.cs
@@ -1,9 +1,17 @@
 ﻿namespace _01_functions;
 
-class Program
+    internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Program Program = new Program();
+        Program.Run();// er word een niewe program gemaakt en daarna gaat hij het program runnen 
+    } // het is een function. het return type function is
+    internal void Run()
+    {
+        Console.WriteLine("hoi");
     }
+    
+
+    // hier komt de function
 }
