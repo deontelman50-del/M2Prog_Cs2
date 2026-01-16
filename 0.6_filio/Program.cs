e38ea3191ffce63ada90de40605f77bf93c986f5
DEON <40478@ma-web.nl>
2026-01-16 14:51:13 +0100

    update

---
+++ 0.6_filio/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+
+namespace _0._6_filio;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World 2!");
+
+        Program program = new Program();
+        program.Run();
+    }
+    void Run()
+    {
+    }
+}
